f86d01881d5e2e053cba4ceb76492a341726799e
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-04-13 00:51:56 +0800

    added datatable

--- app/dashboard/books/page.tsx
+++ app/dashboard/books/page.tsx
@@ -1,13 +1,28 @@
 import { createClient } from "@/utils/supabase/server";
 
-export default async function Books() {
-    const supabase =await createClient();
-    const { data: books} = await supabase.from("books").select();
-    return (
-        <>
-            <div className="container">
-               {books?.map((book)=>(<p key={book.id}>{book.title}</p>))}
-            </div>
-        </>
-    );
+// export default async function Books() {
+//     const supabase =await createClient();
+//     const { data: books} = await supabase.from("books").select();
+//     return (
+//         <>
+//             <div className="container">
+//                {books?.map((book)=>(<p key={book.id}>{book.title}</p>))}
+//             </div>
+//         </>
+//     );
+// }
+
+import {  columns } from "@/components/books/columns";
+import { DataTable } from "@/components/books/data-table";
+
+
+export default async function DemoPage() {
+      const supabase =await createClient();
+      const { data: books} = await supabase.from("books").select();
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={books || []} />
+    </div>
+  );
 }
